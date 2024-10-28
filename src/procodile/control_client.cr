3af32faabf9abde8ae34757ac11ea1a99641d633
Billy.Zheng <vil963@gmail.com>
2024-10-28 13:42:55 +0800

    Add type to control_client.cr

--- src/procodile/control_client.cr
+++ src/procodile/control_client.cr
@@ -3,6 +3,18 @@ require "socket"
 
 module Procodile
   class ControlClient
+    alias SocketResponse = Array(InstanceConfig) |
+                           Array(Tuple(InstanceConfig?, InstanceConfig?)) |
+                           NamedTuple(started: Array(InstanceConfig), stopped: Array(InstanceConfig)) |
+                           ControlClientReplyForStatusCommand | Bool
+
+    def self.run(sock_path : String, command : String, **options) : SocketResponse
+      socket = self.new(sock_path)
+      socket.run(command, **options)
+    ensure
+      socket.try &.disconnect
+    end
+
     def initialize(sock_path, block : Proc(ControlClient, Nil)? = nil)
       @socket = UNIXSocket.new(sock_path)
 
@@ -15,31 +27,23 @@ module Procodile
       end
     end
 
-    def self.run(sock_path : String, command : String, **options)
-      socket = self.new(sock_path)
-      socket.run(command, **options)
-    ensure
-      socket.try &.disconnect
-    end
-
-    def run(command, **options)
+    def run(command, **options) : SocketResponse
       @socket.puts("#{command} #{options.to_json}")
 
       if (data = @socket.gets)
         code, reply = data.strip.split(/\s+/, 2)
-        if code.to_i == 200
-          if reply && !reply.empty?
-            case command
-            when "start_processes", "stop"
-              Array(InstanceConfig).from_json(reply)
-            when "restart"
-              Array(Tuple(InstanceConfig?, InstanceConfig?)).from_json(reply)
-            when "check_concurrency"
-              NamedTuple(started: Array(InstanceConfig), stopped: Array(InstanceConfig)).from_json(reply)
-            when "status"
-              ControlClientReplyForStatusCommand.from_json(reply)
-            end
-          else
+
+        if code.to_i == 200 && reply && !reply.empty?
+          case command
+          when "start_processes", "stop"
+            Array(InstanceConfig).from_json(reply)
+          when "restart"
+            Array(Tuple(InstanceConfig?, InstanceConfig?)).from_json(reply)
+          when "check_concurrency"
+            NamedTuple(started: Array(InstanceConfig), stopped: Array(InstanceConfig)).from_json(reply)
+          when "status"
+            ControlClientReplyForStatusCommand.from_json(reply)
+          else # e.g. reload command
             true
           end
         else
@@ -50,7 +54,7 @@ module Procodile
       end
     end
 
-    def disconnect
+    def disconnect : Nil
       @socket.try &.close
     end
   end
