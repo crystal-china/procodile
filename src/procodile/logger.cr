36ffcf3ef88a47bd2074b17ac87ae6d96d6e5e1a
Billy.Zheng <vil963@gmail.com>
2024-10-31 12:59:31 +0800

    Refine use STDOUT#<< instead of STDOUT.puts

--- src/procodile/logger.cr
+++ src/procodile/logger.cr
@@ -6,9 +6,11 @@ module Procodile
   def self.log(color : Int32?, name : String, text : String)
     mutex.synchronize do
       text.each_line do |message|
-        STDOUT.puts "#{Time.local.to_s("%H:%M:%S")} #{name.ljust(18, ' ')} | ".color(color) + message
-        STDOUT.flush
+        STDOUT << "#{Time.local.to_s("%H:%M:%S")} #{name.ljust(18, ' ')} | ".color(color)
+        STDOUT << message
+        STDOUT << "\n"
       end
+      STDOUT.flush
     end
   end
 end
