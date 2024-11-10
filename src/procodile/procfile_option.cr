edf5cf5d9fc42e38bf0963b023d6859e88108a5e
Billy.Zheng <vil963@gmail.com>
2024-11-10 17:44:13 +0800

    Rename ProcfileOption -> Config::Option

--- src/procodile/procfile_option.cr
+++ src/procodile/procfile_option.cr
@@ -3,23 +3,4 @@ require "json"
 require "../../src/procodile/cli"
 
 module Procodile
-  struct ProcfileOption
-    include YAML::Serializable
-
-    property app_name : String?
-    property root : String?
-    property procfile : String?
-    property pid_root : String?
-    property log_path : String?
-    property log_root : String?
-    property user : String?
-    property console_command : String?
-    property exec_prefix : String?
-    property env : Hash(String, String)?
-    property processes : Hash(String, Process::Option)?
-    property app_id : Process::Option?
-
-    def initialize
-    end
-  end
 end
