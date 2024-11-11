0c03dc24aadc9461e1f04891dc2094676901e948
Billy.Zheng <vil963@gmail.com>
2024-11-11 15:53:39 +0800

    Refactor CLI::Options.reload use property?

--- src/procodile/cli.cr
+++ src/procodile/cli.cr
@@ -181,7 +181,7 @@ module Procodile
       property? start_processes : Bool?
       property? stop_supervisor : Bool?
       property? wait_until_supervisor_stopped : Bool?
-      property reload : Bool?
+      property? reload : Bool?
       property json : Bool?
       property json_pretty : Bool?
       property simple : Bool?
