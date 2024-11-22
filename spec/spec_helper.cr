a42b8fd28b1b732e23b9113a02feb61da9c9e447
Billy.Zheng <vil963@gmail.com>
2024-11-22 13:03:58 +0800

    Fix spec

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,5 @@
+File.touch("Procfile")
+APPS_ROOT = File.expand_path("apps", __DIR__)
 require "spec"
 require "yaml"
 require "../src/procodile"
-APPS_ROOT = File.expand_path("apps", __DIR__)
-
-# require "../src/procodile"
