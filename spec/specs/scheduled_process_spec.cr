92ec41ddbf67409cc5d09bd377878b75315f9013
Billy.Zheng <vil963@gmail.com>
2026-03-23 21:35:24 +0800

    Added spec

--- spec/specs/scheduled_process_spec.cr
+++ spec/specs/scheduled_process_spec.cr
@@ -72,4 +72,78 @@ RUBY
       FileUtils.rm_rf(app_root)
     end
   end
+
+  it "stops future runs and start_processes reenables schedule without running immediately" do
+    app_root = File.join("/tmp", "procodile-scheduled-stop-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "scheduled_task.rb"),
+      <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts "tick"
+end
+RUBY
+    )
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      %Q("job__at__*/1 * * * * *": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+    process = config.processes["job"]
+
+    begin
+      supervisor.start_processes(nil).should be_empty
+
+      output_file = File.join(app_root, "schedule.out")
+
+      wait_until(8.seconds, 100.milliseconds) do
+        File.exists?(output_file) && File.read_lines(output_file).size >= 1
+      end.should be_true
+
+      process.last_finished_at.should_not be_nil
+
+      first_lines = File.read_lines(output_file)
+      first_lines.size.should be >= 1
+
+      supervisor.stop(Procodile::Supervisor::Options.new(processes: ["job"]))
+
+      sleep 2.2.seconds
+
+      File.read_lines(output_file).size.should eq(first_lines.size)
+
+      process.last_started_at.should_not be_nil
+      process.last_finished_at.should_not be_nil
+
+      before_restart_started_at = process.last_started_at
+
+      supervisor.start_processes(["job"]).should be_empty
+
+      process.last_started_at.should eq(before_restart_started_at)
+
+      sleep 0.3.seconds
+
+      File.read_lines(output_file).size.should eq(first_lines.size)
+
+      wait_until(5.seconds, 100.milliseconds) do
+        File.read_lines(output_file).size > first_lines.size
+      end.should be_true
+    ensure
+      File.write(File.join(app_root, "Procfile"), "noop: env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n")
+      supervisor.reload_config
+
+      supervisor.processes.each_value do |instances|
+        instances.each(&.stop)
+      end
+
+      wait_until(5.seconds, 100.milliseconds) do
+        supervisor.processes.values.flatten.none?(&.running?)
+      end
+
+      FileUtils.rm_rf(app_root)
+    end
+  end
 end
