31e2964917aa29b16856d53984a88111903d9744
Billy.Zheng <vil963@gmail.com>
2026-04-04 22:19:09 +0800

    Added spec

---
+++ spec/specs/runtime_issues_spec.cr
@@ -0,0 +1,167 @@
+require "../spec_helper"
+
+private def wait_for_control_socket(sock_path : String) : Bool
+  wait_until(5.seconds, 50.milliseconds) do
+    begin
+      UNIXSocket.new(sock_path).close
+      true
+    rescue Socket::Error | File::Error
+      false
+    end
+  end
+end
+
+private def run_cli_command(app_root : String, *args : String) : {::Process::Status, String}
+  output = IO::Memory.new
+  status = ::Process.run(
+    "crystal",
+    ["run", "src/procodile.cr", "--", "-r", app_root] + args.to_a,
+    output: output,
+    error: output,
+    env: {"CRYSTAL_CACHE_DIR" => "/tmp/crystal-cache"}
+  )
+
+  {status, output.to_s}
+end
+
+describe "runtime issues" do
+  it "prints invalid schedule issues after CLI commands and clears them after reload with a valid schedule" do
+    app_root = File.join("/tmp", "procodile-invalid-schedule-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "scheduled_task.rb"),
+      <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts "tick"
+end
+RUBY
+    )
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      %Q("job__at__*/2 * * * **": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+
+    begin
+      File.write(config.supervisor_pid_path, ::Process.pid.to_s)
+      Procodile::ControlServer.start(supervisor)
+      wait_for_control_socket(config.sock_path).should be_true
+
+      supervisor.start_processes(nil).should be_empty
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.any?(&.type.invalid_schedule?)
+      end.should be_true
+
+      status, output = run_cli_command(app_root, "status")
+      status.success?.should be_true
+      output.should contain("Active issues:")
+      output.should contain("Scheduled process 'job' has invalid cron schedule '*/2 * * * **'")
+
+      File.write(
+        File.join(app_root, "Procfile"),
+        %Q("job__at__*/1 * * * * *": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+      )
+
+      supervisor.reload_config
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.none?(&.type.invalid_schedule?)
+      end.should be_true
+
+      status, output = run_cli_command(app_root, "status")
+      status.success?.should be_true
+      output.should_not contain("Active issues:")
+      output.should_not contain("invalid cron schedule")
+    ensure
+      File.write(File.join(app_root, "Procfile"), "noop: env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n")
+      supervisor.reload_config
+      FileUtils.rm_rf(config.supervisor_pid_path)
+      FileUtils.rm_rf(app_root)
+    end
+  end
+
+  it "reports permanent process failures and clears the issue after a successful restart" do
+    app_root = File.join("/tmp", "procodile-process-failure-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "fail.sh"),
+      <<-'SH'
+#!/usr/bin/env bash
+exit 1
+SH
+    )
+    File.write(
+      File.join(app_root, "ok.sh"),
+      <<-'SH'
+#!/usr/bin/env bash
+sleep 2
+SH
+    )
+    File.chmod(File.join(app_root, "fail.sh"), 0o755)
+    File.chmod(File.join(app_root, "ok.sh"), 0o755)
+
+    File.write(File.join(app_root, "Procfile"), "app1: bash fail.sh\n")
+    File.write(
+      File.join(app_root, "Procfile.options"),
+      <<-'YAML'
+processes:
+  app1:
+    max_respawns: 0
+YAML
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+    process = config.processes["app1"]
+    instance = process.create_instance(supervisor)
+
+    begin
+      File.write(config.supervisor_pid_path, ::Process.pid.to_s)
+      Procodile::ControlServer.start(supervisor)
+      wait_for_control_socket(config.sock_path).should be_true
+
+      instance.start
+
+      wait_until(5.seconds, 50.milliseconds) { !instance.running? }.should be_true
+      instance.check
+
+      wait_until(2.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.any?(&.type.process_failed_permanently?)
+      end.should be_true
+
+      status, output = run_cli_command(app_root, "status")
+      status.success?.should be_true
+      output.should contain("Active issues:")
+      output.should contain("Process 'app1' failed repeatedly and will not be respawned automatically")
+
+      File.write(File.join(app_root, "Procfile"), "app1: bash ok.sh\n")
+      supervisor.restart(Procodile::Supervisor::Options.new(processes: ["app1"]))
+
+      wait_until(2.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.none?(&.type.process_failed_permanently?)
+      end.should be_true
+
+      status, output = run_cli_command(app_root, "status")
+      status.success?.should be_true
+      output.should_not contain("Active issues:")
+      output.should_not contain("failed repeatedly")
+    ensure
+      supervisor.processes.each_value do |instances|
+        instances.each(&.stop)
+      end
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.processes.values.flatten.none?(&.running?)
+      end
+
+      FileUtils.rm_rf(config.supervisor_pid_path)
+      FileUtils.rm_rf(app_root)
+    end
+  end
+end
