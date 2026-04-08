bce6c7d9a96b80b4f62e3b2231ce7628dd114fa0
Billy.Zheng <vil963@gmail.com>
2026-04-08 21:22:30 +0800

    Added more spec

--- spec/specs/runtime_issues_spec.cr
+++ spec/specs/runtime_issues_spec.cr
@@ -486,4 +486,142 @@ YAML
       FileUtils.rm_rf(app_root)
     end
   end
+
+  it "restores a scheduled process with restart after fixing an invalid schedule" do
+    app_root = File.join("/tmp", "procodile-invalid-schedule-restart-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "scheduled_task.rb"),
+      <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts "tick"
+end
+RUBY
+    )
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      %Q("job__AT__*/2 * * * **": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+
+    begin
+      File.write(config.supervisor_pid_path, ::Process.pid.to_s)
+      Procodile::ControlServer.start(supervisor)
+      wait_for_control_socket(config.sock_path).should be_true
+
+      supervisor.start_processes(nil).should be_empty
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.any?(&.type.invalid_schedule?)
+      end.should be_true
+
+      File.write(
+        File.join(app_root, "Procfile"),
+        %Q("job__AT__*/1 * * * * *": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+      )
+
+      restart_status, restart_output = run_cli_command(app_root, "restart", "-p", "job")
+      restart_status.success?.should be_true
+      restart_output.should contain("Reloaded schedule for job.")
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.runtime_issues.none?(&.type.invalid_schedule?)
+      end.should be_true
+
+      output_file = File.join(app_root, "schedule.out")
+      wait_until(5.seconds, 100.milliseconds) do
+        File.exists?(output_file) && !File.read(output_file).empty?
+      end.should be_true
+    ensure
+      File.write(File.join(app_root, "Procfile"), "noop: env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n")
+      supervisor.reload_config
+      FileUtils.rm_rf(config.supervisor_pid_path)
+      FileUtils.rm_rf(app_root)
+    end
+  end
+
+  it "skips removed running processes during full restart" do
+    app_root = File.join("/tmp", "procodile-restart-skips-removed-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "ok.sh"),
+      <<-'SH'
+#!/usr/bin/env bash
+echo $$ >> app.log
+sleep 10
+SH
+    )
+    File.chmod(File.join(app_root, "ok.sh"), 0o755)
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      <<-'PROCFILE'
+app1: bash ok.sh
+app2: bash ok.sh
+PROCFILE
+    )
+    File.write(
+      File.join(app_root, "Procfile.options"),
+      <<-'YAML'
+processes:
+  app1:
+    max_respawns: 0
+  app2:
+    max_respawns: 0
+YAML
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+
+    begin
+      File.write(config.supervisor_pid_path, ::Process.pid.to_s)
+      Procodile::ControlServer.start(supervisor)
+      wait_for_control_socket(config.sock_path).should be_true
+
+      supervisor.start_processes(nil).map(&.process.name).sort!.should eq(["app1", "app2"])
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.processes.values.flatten.count(&.running?) == 2
+      end.should be_true
+
+      removed_instance = supervisor.processes.values.flatten.find { |instance| instance.process.name == "app1" && instance.running? }.not_nil!
+      removed_description = removed_instance.description
+
+      File.write(File.join(app_root, "Procfile"), "app2: bash ok.sh\n")
+      File.write(
+        File.join(app_root, "Procfile.options"),
+        <<-'YAML'
+processes:
+  app2:
+    max_respawns: 0
+YAML
+      )
+      supervisor.reload_config
+
+      restart_status, restart_output = run_cli_command(app_root, "restart")
+      restart_status.success?.should be_true
+      restart_output.should contain("Skipped #{removed_description}, it is still running but has been removed from the Procfile")
+
+      still_running = supervisor.processes.values.flatten.find { |instance| instance.description == removed_description }
+      still_running.should_not be_nil
+      still_running.not_nil!.running?.should be_true
+    ensure
+      supervisor.processes.each_value do |instances|
+        instances.each(&.stop)
+      end
+
+      wait_until(5.seconds, 50.milliseconds) do
+        supervisor.processes.values.flatten.none?(&.running?)
+      end
+
+      FileUtils.rm_rf(config.supervisor_pid_path)
+      FileUtils.rm_rf(app_root)
+    end
+  end
 end
--- spec/specs/scheduled_process_spec.cr
+++ spec/specs/scheduled_process_spec.cr
@@ -324,4 +324,127 @@ RUBY
       FileUtils.rm_rf(app_root)
     end
   end
+
+  it "reports repeated skipped runs and clears the issue after a successful run" do
+    app_root = File.join(Dir.current, "spec/tmp/procodile-scheduled-skip-issue-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "scheduled_task.rb"),
+      <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts "start"
+end
+
+sleep 3.5
+
+File.open("schedule.out", "a") do |file|
+  file.puts "finish"
+end
+RUBY
+    )
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      %Q("job__AT__*/1 * * * * *": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+    process = config.processes["job"]
+
+    begin
+      supervisor.start_processes(nil).should be_empty
+
+      wait_until(8.seconds, 100.milliseconds) do
+        supervisor.runtime_issues.any?(&.type.scheduled_run_skipped_repeatedly?)
+      end.should be_true
+
+      File.write(
+        File.join(app_root, "scheduled_task.rb"),
+        <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts "fast"
+end
+RUBY
+      )
+
+      wait_until(8.seconds, 100.milliseconds) do
+        process.last_finished_at != nil &&
+          supervisor.runtime_issues.none?(&.type.scheduled_run_skipped_repeatedly?)
+      end.should be_true
+    ensure
+      File.write(File.join(app_root, "Procfile"), "noop: env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n")
+      supervisor.reload_config
+
+      supervisor.processes.each_value do |instances|
+        instances.each(&.stop)
+      end
+
+      wait_until(5.seconds, 100.milliseconds) do
+        supervisor.processes.values.flatten.none?(&.running?)
+      end
+
+      FileUtils.rm_rf(app_root)
+    end
+  end
+
+  it "does not restart a running scheduled instance during full restart" do
+    app_root = File.join(Dir.current, "spec/tmp/procodile-scheduled-full-restart-#{Random.rand(1_000_000)}")
+    FileUtils.mkdir_p(File.join(app_root, "pids"))
+
+    File.write(
+      File.join(app_root, "scheduled_task.rb"),
+      <<-'RUBY'
+File.open("schedule.out", "a") do |file|
+  file.puts Process.pid
+end
+
+sleep 3
+RUBY
+    )
+
+    File.write(
+      File.join(app_root, "Procfile"),
+      %Q("job__AT__*/1 * * * * *": env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n)
+    )
+
+    config = Procodile::Config.new(root: app_root)
+    supervisor = Procodile::Supervisor.new(config)
+
+    begin
+      supervisor.start_processes(nil).should be_empty
+
+      output_file = File.join(app_root, "schedule.out")
+      wait_until(8.seconds, 100.milliseconds) do
+        File.exists?(output_file) && File.read_lines(output_file).size >= 1 &&
+          supervisor.processes.values.flatten.any? { |instance| instance.process.name == "job" && instance.running? }
+      end.should be_true
+
+      first_run_count = File.read_lines(output_file).size
+      running_instance = supervisor.processes.values.flatten.find { |instance| instance.process.name == "job" && instance.running? }.not_nil!
+      first_description = running_instance.description
+
+      supervisor.restart
+
+      sleep 0.5.seconds
+
+      File.read_lines(output_file).size.should eq(first_run_count)
+      current_running_instance = supervisor.processes.values.flatten.find { |instance| instance.process.name == "job" && instance.running? }.not_nil!
+      current_running_instance.description.should eq(first_description)
+    ensure
+      File.write(File.join(app_root, "Procfile"), "noop: env -u RUBYOPT -u RUBYLIB ruby scheduled_task.rb\n")
+      supervisor.reload_config
+
+      supervisor.processes.each_value do |instances|
+        instances.each(&.stop)
+      end
+
+      wait_until(5.seconds, 100.milliseconds) do
+        supervisor.processes.values.flatten.none?(&.running?)
+      end
+
+      FileUtils.rm_rf(app_root)
+    end
+  end
 end
